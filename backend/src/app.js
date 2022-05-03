b3d6bd896403afc50e4324a7f419070ff48eb194
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2022-05-02 19:02:33 -0500

    knex add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
+const knex = require("./db/connection")
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
-const cors = require("cors");
-const notFound = require("./errors/notFound");
-const errorHandler = require("./errors/errorHandler");
-
 const app = express();
 
-let corsOptions = {
+/*let corsOptions = {
     origin: 'https://chris-welovemovies-backend.herokuapp.com',
     optionsSuccessStatus: 200, // For legacy browser support
     methods: "GET, PUT, DELETE"
-}
+}*/
+//Trying to fix cors issue, got it at this url "https://stackabuse.com/handling-cors-with-node-js/"
+//must also use: app.use(cors(corsOptions))
 
-app.use(cors(corsOptions));
+app.set("db", knex);
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
